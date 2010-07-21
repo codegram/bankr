228427c22c769777848c0de0bb819151ba9c660a
Josep Mª Bach <josep.m.bach@gmail.com>
2010-07-21 17:44:36 +0200

    Included ActiveModel in Account and Movement

--- lib/bankr.rb
+++ lib/bankr.rb
@@ -1,3 +1,4 @@
+require 'active_model'
 require 'bankr/bankr'
 require 'bankr/account'
 require 'bankr/movement'
--- lib/bankr/movement.rb
+++ lib/bankr/movement.rb
@@ -2,9 +2,16 @@ module Bankr
 
   class Movement
 
+    include ::ActiveModel::Validations
+    include ::ActiveModel::Serialization
+
+    validates_presence_of :account, :statement, :amount, :date
+
     attr_accessor :account, :statement, :amount, :date
 
     def initialize(options)
+      @attributes = options
+
       @account = options[:account] 
       @statement = options[:statement] 
       @amount = options[:amount] 
