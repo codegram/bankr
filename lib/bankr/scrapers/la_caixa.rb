b332e427d9447063085c7fb361e866c058d4f541
Oriol Gual <oriol.gual@gmail.com>
2016-04-28 15:37:15 +0200

    LOL there's no initial body

--- lib/bankr/scrapers/la_caixa.rb
+++ lib/bankr/scrapers/la_caixa.rb
@@ -97,7 +97,6 @@ module Bankr
       end
 
       def inside_main_iframe(&block)
-        session.find('body')
         session.within_frame('Inferior') do
           session.find('body')
           session.within_frame('Cos') do
